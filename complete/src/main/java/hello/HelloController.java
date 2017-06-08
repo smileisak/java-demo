2b434dd497c23e938f84ca2248a1ab6b164ced94
Ismail KABOUBI <ikaboubi@gmail.com>
2017-06-08 14:41:40 +0200

    add k8s manifest files

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -8,7 +8,8 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello from bnp bank !";
+        String page = "<!doctype html><title>Kubernetes java demo</title><style>  body {  background-color: yellow; text-align: center; padding: 150px; }  h1 { font-size: 50px; }  body { font: 20px Helvetica, sans-serif; color: #333; }  article { display: block; text-align: left; width: 650px; margin: 0 auto; }  a { color: #dc8100; text-decoration: none; }  a:hover { color: #333; text-decoration: none; }</style><article>    <h1>Demo Java within Kubernetes Cluster</h1>    <div>        <p> Hello from Spring kubernetes version : 2 ! </p>  </div></article>";
+        return page;
     }
 
 }
